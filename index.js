1c3755b0f777b9d962c36e2e51d38394755971fe
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-01 22:10:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import http from 'http';
-import express from 'express';
+const http = require('http');
+const express = require('express');
+const ws = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Running on port ${PORT}`));
+const webSocket = new ws.Server({ server });
+
+webSocket.on('connection', (socket, request) => {
+    console.log('new connection');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+server.listen(PORT, () => console.log(`Running on -> http://localhost:${PORT} ...`));
 
