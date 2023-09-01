1893caf6590069507f5cd2d4dcc290af8ae837cd
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-01 21:55:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import http from 'http';
 import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Running on port ${PORT}`));
 
